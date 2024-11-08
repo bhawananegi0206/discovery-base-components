702390bcef6a9b840f0cf434ade1aee2827bf83b
Matei87 <mrc_matei@yahoo.com>
2024-11-08 15:30:31 +0200

    added hero and double hero sections responsive

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,8 @@
 // public/js/main.js
 document.addEventListener('DOMContentLoaded', () => {
+  loadComponent('header', 'components/header.html');
   loadComponent('hero', 'components/hero/hero.html');
   loadComponent('double-hero', 'components/double-hero/double-hero.html');
-  loadComponent('header', 'components/header.html');
   loadComponent('main-content', 'components/mainContent/mainContent.html');
   loadComponent('footer', 'components/footer.html');
 });
